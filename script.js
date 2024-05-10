785fe55b77480a8ba2deb5be5ac877de7bdd09c5
thinun <75312054+thinun@users.noreply.github.com>
2024-05-10 21:31:19 +0530

    hamberger menu added

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-// Script for navigation bar
+const bar = document.querySelector('#mobile');
+const navbar = document.querySelector('#navbar');
+const body = document.querySelector('body');
+
+bar.addEventListener('click', e => {
+    navbar.classList.toggle('active');
+    e.stopPropagation(); // Prevent the click event from propagating to the body
+});
+
+body.addEventListener('click', e => {
+    if (!navbar.contains(e.target)) { // Check if the clicked element is not part of the navbar
+        navbar.classList.remove('active');
+    }
+});
