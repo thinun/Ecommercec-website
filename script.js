d9c144da165174694049bc56901690fb7acbf13b
thinun <75312054+thinun@users.noreply.github.com>
2024-05-11 12:04:12 +0530

    media queries added

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 const bar = document.querySelector('#mobile');
 const navbar = document.querySelector('#navbar');
-const body = document.querySelector('body');
 
+const closeBtn = document.querySelector('#close-ham');
 bar.addEventListener('click', e => {
     navbar.classList.toggle('active');
     e.stopPropagation(); // Prevent the click event from propagating to the body
 });
 
-body.addEventListener('click', e => {
-    if (!navbar.contains(e.target)) { // Check if the clicked element is not part of the navbar
+closeBtn.addEventListener('click', e => {
+    if (closeBtn.contains(e.target)) {
         navbar.classList.remove('active');
     }
 });
